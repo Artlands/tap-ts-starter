285aa8ca7ae3e549333665e1eed62f1eaf7e0034
Artlands <artlands@users.noreply.github.com>
2018-06-22 10:37:56 -0500

    before deployment

---
+++ src/parse-csv.ts
@@ -0,0 +1,17 @@
+/*Parse CSV files using csvjson https://www.npmjs.com/package/csvjson */
+
+import * as tapTypes from './singer/tap-types'
+let csvjson = require('csvjson')
+let options = {
+  // delimiter: ',', // optional
+  // quote: '"' // optional
+}
+
+export function parseItem(csvfile: Buffer) {
+  let jsonobj = csvjson.toObject(csvfile.toString())
+  let rec = new tapTypes.streamRecord()
+  rec.stream = 'csv'
+  rec.time_extracted = new Date()
+  rec.record = jsonobj
+  return rec
+}
